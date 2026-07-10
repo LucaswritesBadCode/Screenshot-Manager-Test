1fa69fdce1530d11ecbb12e6dff3a9f37996ed54
LucaswritesBadCode <fourschoolguitar@gmail.com>
2026-07-10 12:28:28 +0800

    test: added tests for bowlingscorecalculator
    to test how it handles strikes, spare, the final frame and error handling

--- Assets/LucasWritesBadCode/BowlingScoreCounter/Tests/Editor/BowlingScoreCalculatorTestsv2.cs
+++ Assets/LucasWritesBadCode/BowlingScoreCounter/Tests/Editor/BowlingScoreCalculatorTestsv2.cs
@@ -10,7 +10,7 @@ namespace LucasWritesBadCode.BowlingScoreCounter.Tests.Editor
         [Test]
         public void When_FirstRound5_Expect_5()
         {
-            string[] pins = { "5" };
+            string[] pins = { "5 0" };
             ScoreResult result = ScoreHandler.GetScoreResult(pins);
 
             Assert.That(result.TotalScore, Is.EqualTo(5));
@@ -26,9 +26,54 @@ namespace LucasWritesBadCode.BowlingScoreCounter.Tests.Editor
         }
 
         [Test]
+        public void When_Spare_Expect_10()
+        {
+            string[] pins = { "3 /" };
+            ScoreResult result = ScoreHandler.GetScoreResult(pins);
+
+            Assert.That(result.TotalScore, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void When_StrikeThen2And3_Expect_20()
+        {
+            string[] pins = { "X", "2 3" };
+            ScoreResult result = ScoreHandler.GetScoreResult(pins);
+
+            Assert.That(result.TotalScore, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void When_SpareThen2And3_Expect_17()
+        {
+            string[] pins = { "2 /", "2 3" };
+            ScoreResult result = ScoreHandler.GetScoreResult(pins);
+
+            Assert.That(result.TotalScore, Is.EqualTo(17));
+        }
+
+        [Test]
+        public void When_Double_Expect_30()
+        {
+            string[] pins = { "X", "X" };
+            ScoreResult result = ScoreHandler.GetScoreResult(pins);
+
+            Assert.That(result.TotalScore, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void When_Turkey_Expect_60()
+        {
+            string[] pins = { "X", "X", "X" };
+            ScoreResult result = ScoreHandler.GetScoreResult(pins);
+
+            Assert.That(result.TotalScore, Is.EqualTo(60));
+        }
+
+        [Test]
         public void When_Cummulative20_Expect_20()
         {
-            string[] pins = { "4", "6", "5", "3", "2" };
+            string[] pins = { "2 2", "6 0", "2 4", "0 3", "1 0" };
             ScoreResult result = ScoreHandler.GetScoreResult(pins);
 
             Assert.That(result.TotalScore, Is.EqualTo(20));
@@ -37,22 +82,22 @@ namespace LucasWritesBadCode.BowlingScoreCounter.Tests.Editor
         [Test]
         public void When_HasNumberOfPinsOutsideOfMinBound_Expect_Error()
         {
-            int[] pins = { 5, -1 };
-            Assert.Throws<Exception>(() => ScoreHandlerOriginal.CalculateScoreResult(pins));
+            string[] pins = { "5 3", "-1" };
+            Assert.Throws<Exception>(() => ScoreHandler.GetScoreResult(pins));
         }
 
         [Test]
         public void When_HasNumberOfPinsOutsideOfMaxBound_Expect_Error()
         {
-            int[] pins = { 5, 12 };
-            Assert.Throws<Exception>(() => ScoreHandlerOriginal.CalculateScoreResult(pins));
+            string[] pins = { "5 2", "12 4" };
+            Assert.Throws<Exception>(() => ScoreHandler.GetScoreResult(pins));
         }
 
         [Test]
         public void When_MoreThanMaxRound_Expect_IgnoreTheRest()
         {
-            int[] pins = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-            ScoreResult result = ScoreHandlerOriginal.CalculateScoreResult(pins);
+            string[] pins = { "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1" };
+            ScoreResult result = ScoreHandler.GetScoreResult(pins);
 
             Assert.That(result.TotalScore, Is.EqualTo(10));
         }
@@ -60,8 +105,8 @@ namespace LucasWritesBadCode.BowlingScoreCounter.Tests.Editor
         [Test]
         public void When_AllStrikes_Expect_300()
         {
-            int[] pins = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
-            ScoreResult result = ScoreHandlerOriginal.CalculateScoreResult(pins);
+            string[] pins = { "X", "X", "X", "X", "X", "X", "X", "X", "X", "XXX" };
+            ScoreResult result = ScoreHandler.GetScoreResult(pins);
 
             Assert.That(result.TotalScore, Is.EqualTo(300));
         }
@@ -69,8 +114,8 @@ namespace LucasWritesBadCode.BowlingScoreCounter.Tests.Editor
         [Test]
         public void When_NoPins_Expect_0()
         {
-            int[] pins = { 0, 0, 0, 0, 0 };
-            ScoreResult result = ScoreHandlerOriginal.CalculateScoreResult(pins);
+            string[] pins = { "0", "0", "0", "0", "0" };
+            ScoreResult result = ScoreHandler.GetScoreResult(pins);
 
             Assert.That(result.TotalScore, Is.EqualTo(0));
         }
@@ -78,16 +123,60 @@ namespace LucasWritesBadCode.BowlingScoreCounter.Tests.Editor
         [Test]
         public void When_EmptyInput_Expect_0()
         {
-            int[] pins = { };
-            ScoreResult result = ScoreHandlerOriginal.CalculateScoreResult(pins);
+            string[] pins = { };
+            ScoreResult result = ScoreHandler.GetScoreResult(pins);
 
             Assert.That(result.TotalScore, Is.EqualTo(0));
         }
 
         [Test]
+        public void When_SpareOnFirstAttempt_Expect_Error()
+        {
+            string[] pins = { "/ 2" };
+            Assert.Throws<Exception>(() => ScoreHandler.GetScoreResult(pins));
+        }
+
+        [Test]
+        public void When_StrikeOnSecondAttempt_Expect_Error()
+        {
+            string[] pins = { "5 X" };
+            Assert.Throws<Exception>(() => ScoreHandler.GetScoreResult(pins));
+        }
+
+        [Test]
+        public void When_HaveAttemptAfterStrike_Expect_Error()
+        {
+            string[] pins = { "X 5" };
+            Assert.Throws<Exception>(() => ScoreHandler.GetScoreResult(pins));
+        }
+
+        [Test]
+        public void When_InvalidSymbol_Expect_Error()
+        {
+            string[] pins = { "S" };
+            Assert.Throws<Exception>(() => ScoreHandler.GetScoreResult(pins));
+        }
+
+        [Test]
         public void When_NullInput_Expect_NullException()
         {
-            Assert.Throws<ArgumentNullException>(() => ScoreHandlerOriginal.CalculateScoreResult(null));
+            Assert.Throws<ArgumentNullException>(() => ScoreHandler.GetScoreResult(null));
+        }
+
+        [Test]
+        public void When_FinalFrameAllStrikes_Expect_30()
+        {
+            string[] pins = { "0", "0", "0", "0", "0", "0", "0", "0", "0", "XXX" };
+            ScoreResult result = ScoreHandler.GetScoreResult(pins);
+
+            Assert.That(result.TotalScore, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void When_FinalFrameHas3AttemptsWithoutStrikeOrSpare_Expect_Error()
+        {
+            string[] pins = { "0", "0", "0", "0", "0", "0", "0", "0", "0", "324" };
+            Assert.Throws<Exception>(() => ScoreHandler.GetScoreResult(pins));
         }
     }
 }
